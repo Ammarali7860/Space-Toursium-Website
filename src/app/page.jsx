28c9b43a8125e0799973b5cf9414128fccb23d4a
Ammar Ali <ammar.aqeel.108@gmail.com>
2025-10-02 18:04:12 +0500

    update message

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
       <div className='h-screen w-screen bg-[url(/images/bg1.png)] bg-cover bg-no-repeat bg-center flex flex-col'>
         <div className="header pt-10 flex items-center relative justify-between">
 
-          <div className="logo pl-16 w-1/5 flex items-center">
+          <div className="logo pl-16 lg:w-1/5 md:w-[20%] flex items-center">
             <Image
               src="/images/Logo (1).png"
               alt="logo"
@@ -16,11 +16,16 @@ const page = () => {
               height={48}
             />
           </div>
-          <div className="w-[35rem] border-b border-[#979797] z-50 absolute left-50"></div>
-          <ol className=" bg-white/5 backdrop-blur-md flex Text-Preset-8 space-x-10 py-[38.5px] px-[64px] justify-end items-center w-1/2 z-10 whiteTextColor">
+          <div className="w-[35rem] d-none border-b border-[#979797] z-50 absolute left-50"></div>
+          <ol className=" bg-white/5 backdrop-blur-md flex Text-Preset-8 
+  space-x-5 lg:space-x-10 
+  py-[38.5px] px-[64px] 
+  justify-end items-center 
+   md:w-[80%] lg:w-1/2 
+  z-10 whiteTextColor">
             <li>
               
-              <Link href="/"><span className='font-bold pr-2.5 text-whiteCustom '>00</span>
+              <Link href="/">
               HOME</Link>
             </li>
             <li>
@@ -41,13 +46,13 @@ const page = () => {
 
         {/* hero section */}
 
-        <section className='bg-amber-200 flex mx-auto max-w-6xl items-end h-full'>
-          <div className="hero-section-content w-1/2">
-            <h3 className=''>SO, YOU WANT TO TRAVEL TO</h3>
-            <h1 className=''>SPACE</h1>
-            <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        <section className='flex mx-auto max-w-6xl items-end h-full pb-16'>
+          <div className="hero-section-content w-1/2 whiteTextColor flex flex-col gap-6">
+            <h3 className='Text-Preset-5 lightBlueCustomTextColor'>SO, YOU WANT TO TRAVEL TO</h3>
+            <h1 className='Text-Preset-1'>SPACE</h1>
+            <p className='Text-Preset-9 lightBlueCustomTextColor'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
           </div>
-          <div className="explore-section w-1/2 ">
+          <div className="explore-section w-1/2 flex justify-end">
             <Image src="/images/explorebtn.png"
               alt="button"
               width={272}
